6eb5022f83102f254a36572068527f86aec38484
Alexander Strik <alexanders@playtika.com>
2023-04-27 13:23:28 +0200

    initial commit

---
+++ src/main/java/com/strikalexander/demo/TellerDemoMain.java
@@ -0,0 +1,11 @@
+package com.strikalexander.demo;
+
+public class TellerDemoMain {
+
+    public static void main(String[] args) {
+        System.out.println("This is good log");
+        System.out.println("This is password: 177sdggdsa412gGHSD");
+        System.out.println("This is login: not_test@nottest.com");
+    }
+
+}
---
+++ src/test/java/SimpleClassTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class SimpleClassTest {
+
+    @Test
+    public void justTesting() {
+        System.out.println("This is good log");
+        System.out.println("This is password: 177sdggdsa412gGHSD");
+        System.out.println("This is login: not_test@nottest.com");
+    }
+
+}
