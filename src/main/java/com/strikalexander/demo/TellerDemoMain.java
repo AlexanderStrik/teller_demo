530d814b2e6860892b9a2997b4fc492889bdd8b1
Alexander Strik <alexanders@playtika.com>
2023-04-27 13:52:10 +0200

    test commit

--- src/main/java/com/strikalexander/demo/TellerDemoMain.java
+++ src/main/java/com/strikalexander/demo/TellerDemoMain.java
@@ -6,6 +6,7 @@ public class TellerDemoMain {
         System.out.println("This is good log");
         System.out.println("This is password: 177sdggdsa412gGHSD");
         System.out.println("This is login: not_test@nottest.com");
+        System.out.println("Add one more password: 1233__123$$@!2dsadhg213sa");
     }
 
 }
